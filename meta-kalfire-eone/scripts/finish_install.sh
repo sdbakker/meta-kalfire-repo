973bcf90dedcda3594499114bb9d99b2d8ae9a28
Simon de Bakker <simon@commonplace.nl>
2020-06-11 16:29:06 +0200

    made finish_install.sh executable

--- meta-kalfire-eone/scripts/finish_install.sh
+++ meta-kalfire-eone/scripts/finish_install.sh
@@ -4,4 +4,4 @@ build_dir=$1
 
 if [ -f "${build_dir}/../sources/meta-laird-cp/recipes-packages/openssl/openssl_1.0.%.bbappend" ]; then
     rm ${build_dir}/../sources/meta-laird-cp/recipes-packages/openssl/openssl_1.0.%.bbappend
-]
+fi
